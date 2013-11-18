0612b7c75ccb517c310c06c53de5002ae6b9a1b3
Edd Porter <github@eddporter.com>
2013-11-18 07:46:58 +0000

    Formatting and comments

--- GeneCodeCS/BotCompileException.cs
+++ GeneCodeCS/BotCompileException.cs
@@ -29,23 +29,20 @@ namespace GeneCodeCS
     /// <summary>
     ///   Initializes a new instance of the <see cref="T:System.Exception" /> class.
     /// </summary>
-    public BotCompileException() {
-    }
+    public BotCompileException() { }
 
     /// <summary>
     ///   Initializes a new instance of the <see cref="T:System.Exception" /> class with a specified error message.
     /// </summary>
     /// <param name="message"> The message that describes the error. </param>
-    public BotCompileException(string message) : base(message) {
-    }
+    public BotCompileException(string message) : base(message) { }
 
     /// <summary>
     ///   Initializes a new instance of the <see cref="T:System.Exception" /> class with a specified error message and a reference to the inner exception that is the cause of this exception.
     /// </summary>
     /// <param name="message"> The error message that explains the reason for the exception. </param>
     /// <param name="innerException"> The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified. </param>
-    public BotCompileException(string message, Exception innerException) : base(message, innerException) {
-    }
+    public BotCompileException(string message, Exception innerException) : base(message, innerException) { }
 
     /// <summary>
     ///   Initializes a new instance of the <see cref="T:System.Exception" /> class with serialized data.
@@ -58,7 +55,6 @@ namespace GeneCodeCS
     /// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is null or
     ///   <see cref="P:System.Exception.HResult" />
     ///   is zero (0).</exception>
-    protected BotCompileException(SerializationInfo info, StreamingContext context) : base(info, context) {
-    }
+    protected BotCompileException(SerializationInfo info, StreamingContext context) : base(info, context) { }
   }
 }
